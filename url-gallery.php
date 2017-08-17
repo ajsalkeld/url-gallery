bf182c958f91e8a8d8beeaa73c97f3296f729883
AJ Salkeld <andrew@ajsalkeld.com>
2017-08-17 18:09:13 +0100

    Initial build

---
+++ url-gallery.php
@@ -0,0 +1,40 @@
+<?php
+/*
+   Plugin Name: URL Gallery
+   Plugin URI: https://www.ajsalkeld.com
+   Description: A plugin to make galleries with their URLs.
+   Version: 1.0
+   Author: Andrew Salkeld
+   Author URI: https://www.ajsalkeld.com
+   License: GPL2
+   */
+function ug_get_image_id($image_url) {
+	global $wpdb;
+	$sgattachment = $wpdb->get_col($wpdb->prepare("SELECT ID FROM $wpdb->posts WHERE guid='%s';", $image_url ));
+	return $sgattachment[0];
+}
+add_shortcode('old-gal', 'url_gallery');
+add_shortcode('url-gallery', 'url_gallery');
+function url_gallery($atts){
+	extract( shortcode_atts( array(
+		'imgs' => '',
+		'ids' => '',
+	), $atts, 'old-gal' ) );
+	extract( shortcode_atts( array(
+		'imgs' => '',
+		'ids' => '',
+	), $atts, 'url-gallery' ) );
+
+	if ($atts['imgs'] != null) {
+		$image_names = explode(", ",$atts['imgs']);
+		foreach($image_names as &$image_url) {
+			$image_url = ug_get_image_id($image_url);
+		}
+		$image_names = implode(', ', $image_names);
+		echo do_shortcode('[gallery ids="'.$image_names.'"]');
+	}
+	elseif ($atts['ids'] != null) {
+		echo do_shortcode('[gallery ids="'.$atts['ids'].'"]');
+	}
+
+}
